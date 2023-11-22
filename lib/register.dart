67a5f97e80330b25fb9d1002ef38eda30e6edae6
Bishal <bishal.sen@patancollege.edu.np>
2023-11-22 11:19:23 +0545

    new register and test created

---
+++ lib/register.dart
@@ -0,0 +1,13 @@
+class register extends StatefulWidget {
+  const register({super.key});
+
+  @override
+  State<register> createState() => _registerState();
+}
+
+class _registerState extends State<register> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
